491a7d391db8602ca14d56dff42befda5ea8071d
stanvonderhtlwienwestlol <dusek.s03@htlwienwest.at>
2022-02-08 18:11:18 +0100

    Added Operator: Vector Component Sum Added Operator: Vectoe Component Product

--- src/main/java/stanuwu/missingmagic/spellcomp/RegisterPieces.java
+++ src/main/java/stanuwu/missingmagic/spellcomp/RegisterPieces.java
@@ -6,6 +6,8 @@ import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import stanuwu.missingmagic.MissingMagic;
+import stanuwu.missingmagic.spellcomp.operators.OperatorVectorComponentProduct;
+import stanuwu.missingmagic.spellcomp.operators.OperatorVectorComponentSum;
 import stanuwu.missingmagic.spellcomp.tricks.*;
 import vazkii.psi.api.PsiAPI;
 import vazkii.psi.api.spell.SpellPiece;
@@ -23,6 +25,9 @@ public class RegisterPieces {
         register("trick_torrent_sequence", TrickTorrentSequence.class, false);
         register("trick_melt", TrickMelt.class, false);
         register("trick_melt_sequence", TrickMeltSequence.class, false);
+
+        register("operator_vector_component_sum", OperatorVectorComponentSum.class, false);
+        register("operator_vector_component_product", OperatorVectorComponentProduct.class, false);
     }
     public static void register(String id, Class<? extends SpellPiece> piece, boolean main) {
         PsiAPI.registerSpellPieceAndTexture(new ResourceLocation(MissingMagic.modId, id), piece);
---
+++ src/main/java/stanuwu/missingmagic/spellcomp/operators/OperatorVectorComponentSum.java
@@ -0,0 +1,36 @@
+package stanuwu.missingmagic.spellcomp.operators;
+
+import vazkii.psi.api.internal.Vector3;
+import vazkii.psi.api.spell.Spell;
+import vazkii.psi.api.spell.SpellContext;
+import vazkii.psi.api.spell.SpellParam;
+import vazkii.psi.api.spell.SpellRuntimeException;
+import vazkii.psi.api.spell.param.ParamVector;
+import vazkii.psi.api.spell.piece.PieceOperator;
+
+public class OperatorVectorComponentSum extends PieceOperator {
+
+    SpellParam<Vector3> vec1;
+
+    public OperatorVectorComponentSum(Spell spell) {
+        super(spell);
+    }
+
+    @Override
+    public void initParams() {
+        addParam(vec1 = new ParamVector(SpellParam.GENERIC_NAME_TARGET, SpellParam.BLUE, false, false));
+    }
+
+    @Override
+    public Object execute(SpellContext context) throws SpellRuntimeException {
+        Vector3 v1 = this.getParamValue(context, vec1);
+
+        return v1.x + v1.y + v1.z;
+    }
+
+    @Override
+    public Class<?> getEvaluationType() {
+        return Double.class;
+    }
+
+}
